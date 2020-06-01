ae4bc80f3bec34d77324352f88903ed2c0d10333
Russ Perry <rperry1886@gmail.com>
2020-05-31 20:03:17 -0400

    Add responsive nav

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
   $("#navButton").click(() => {
+    $("#navigation").removeClass("navClosed");
     $("#navigation").addClass("navOpen");
   });
   $("#close").click(() => {
     $("#navigation").removeClass("navOpen");
+    $("#navigation").addClass("navClosed");
   });
 });
